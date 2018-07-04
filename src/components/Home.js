603a001675a6318a5c4b74bade072d88969349b1
Catherine Lin <catherine.lin.93@gmail.com>
2018-07-04 00:11:41 -0700

    added home page w animation

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="text-box">
+        <h1 className="heading-primary">Prickles</h1>
+        <p className="heading-sub">The botique store for plants</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
